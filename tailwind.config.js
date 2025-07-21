fd70d3f2642d1b4077865598ddd2affec6e95e85
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-21 20:14:49 +0800

    feat (tailwind.config.js): include components as a content and add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#7800D3",
+                secondary: "#DDDDDD",
+                primaryBlue: "#006FFF",
+                danger: "#FF2121",
+                darkBg: "#01000F",
+                lightBg: "#F1F5FD",
+                darkGray: "#32322F",
+                lightWhite: "#808080",
+                arsenic: "#1F242A",
+                darkLayer: "#161B21",
+                grayLayer: "#EEF0F1",
+                divider: "#585858",
+                lightGray: "#E9E9E9",
+                success: "#3DC13C",
+                info: "#3598DB",
+                active: "#6EDB3F",
+                warning: "#F39C11",
+                error: "#E84C3D",
+                idle: "#9E9E9E",
+            },
+            fontFamily: {
+                poppins: ["Poppins"],
+                poppinsRegular: ["Poppins-Regular"],
+                poppinsMedium: ["Poppins-Medium"],
+                poppinsBold: ["Poppins-Bold"],
+                pulicsansLight: ["PublicSans-Light"],
+                publicsans: ["PublicSans-Regular"],
+                publicsansBold: ["PublicSans-Bold"],
+            },
+        },
     },
     plugins: [],
 };
