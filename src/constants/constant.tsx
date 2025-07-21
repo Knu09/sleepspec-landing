f90956f277f88e4be56143d162a10befbaf1e58c
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-21 21:39:28 +0800

    feat (constants): navigation items

---
+++ src/constants/constant.tsx
@@ -0,0 +1,6 @@
+export const navItems = [
+    { label: "Features", href: "#" },
+    { label: "How It Works", href: "#" },
+    { label: "Model", href: "#" },
+    { label: "Contact", href: "#" },
+];
