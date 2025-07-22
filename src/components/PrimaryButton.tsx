e1ef2beeaf1a077e7d04086d10bad7a00a8ea8e3
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-22 22:15:26 +0800

    style (PrimaryButton.tsx): update position z-index

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,19 +1,21 @@
 import androidIcon from "../assets/android-icon.png";
 
 type PrimaryButtonProps = {
-    title: string;
+    text: string;
+    href: string;
 };
 
-const PrimaryButton = ({ title }: PrimaryButtonProps) => {
+const PrimaryButton = ({ text, href }: PrimaryButtonProps) => {
     return (
-        <button
-            className="flex flex-row items-center gap-3 px-6 py-3 rounded-full"
+        <a
+            href={href}
+            className="z-30 flex flex-row items-center gap-3 px-6 py-3 rounded-full"
             id="primary-btn"
             type="button"
         >
             <img src={androidIcon} alt="android" />
-            <span>{title}</span>
-        </button>
+            <span className="text-lg font-publicsans font-medium">{text}</span>
+        </a>
     );
 };
 
