9c91be62ff084b92955c62b4d636fb20b781e4d8
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-22 22:14:20 +0800

    feat (Navbar.tsx): drawer content

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,9 @@ import logo from "../assets/logo_sleepspec.png";
 import downloadIcon from "../assets/download-icon.png";
 import { navItems } from "../constants/constant.tsx";
 import { useState } from "react";
+import PrimaryButton from "./PrimaryButton.tsx";
+import SecondaryButton from "./SecondaryButton.tsx";
+
 const Navbar = () => {
     const [drawerOpen, setDrawerOpen] = useState(false);
     const toggleNavbar = () => {
@@ -43,21 +46,39 @@ const Navbar = () => {
                                 alt="download"
                             />
                             <span className="text-lg font-poppins">
-                                Download App
+                                Get the App
                             </span>
                         </a>
                     </div>
-                    <div className="md:hidden flex flex-col">
+                    <div className="md:hidden flex flex-col z-30">
                         <button className="text-lg" onClick={toggleNavbar}>
                             {drawerOpen ? (
-                                <i className="fa-solid fa-bars"></i>
-                            ) : (
                                 <i className="fa-solid fa-xmark"></i>
+                            ) : (
+                                <i className="fa-solid fa-bars"></i>
                             )}
                         </button>
                     </div>
                 </div>
             </div>
+
+            {drawerOpen && (
+                <div className="fixed top-0 right-0 z-20 bg-darkLayer w-full sm:w-9/12 h-screen p-12 flex flex-col justify-center items-center lg:hidden">
+                    <ul className="flex flex-col gap-6">
+                        {navItems.map((item, index) => (
+                            <li className="text-center" key={index}>
+                                <a href={item.href}>{item.label}</a>
+                            </li>
+                        ))}
+                        <li>
+                            <PrimaryButton text="Download APK" href="#" />
+                        </li>
+                        <li>
+                            <SecondaryButton text="Learn More" href="#" />
+                        </li>
+                    </ul>
+                </div>
+            )}
         </nav>
     );
 };
