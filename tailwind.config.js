ed8de70a4408bd5f6cd164878e94f3cd2c6bf8dd
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-22 22:49:37 +0800

    update (tailwind.config.js): include custom CSS in content

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
         "./src/components/**/*.{js,jsx,ts,tsx}",
+        "./src/css/**/*.{css}",
     ],
     theme: {
         extend: {
