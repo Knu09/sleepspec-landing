01f1988e30277c03348e955f1b0de177275498c7
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-22 22:15:46 +0800

    feat (SecondaryButton.tsx)

---
+++ src/components/SecondaryButton.tsx
@@ -0,0 +1,19 @@
+type SecondaryButtonProps = {
+    text: string;
+    href: string;
+};
+
+const SecondaryButton = ({ text, href }: SecondaryButtonProps) => {
+    return (
+        <a
+            href={href}
+            className="z-30 flex flex-row justify-center items-center gap-3 px-6 py-3 rounded-full"
+            id="secondary-btn"
+            type="button"
+        >
+            <span className="text-lg font-publicsans font-medium">{text}</span>
+        </a>
+    );
+};
+
+export default SecondaryButton;
