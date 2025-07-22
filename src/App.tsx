ad4006fdfd8d52da1af8a1e4a2fef616be098192
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-22 22:51:49 +0800

    feat (App.tsx): add FeatureSection to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
 import Navbar from "./components/Navbar";
+
 function App() {
     return (
         <>
             <Navbar />
             <section>
                 <HeroSection />
+                <FeatureSection />
             </section>
         </>
     );
