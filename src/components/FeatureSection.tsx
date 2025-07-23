ec5e65cc2884ec3902948c1bc6c7ad77ccdd4a62
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-23 21:59:06 +0800

    feat (FeatureSection.tsx): add feature items

--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -1,22 +1,42 @@
+import { featItems } from "../constants/constant";
+
 const FeatureSection = () => {
     return (
         <div className="bg-white text-darkBg">
             <div className="container mx-auto py-20">
-                <div className="flex flex-col">
+                <div className="flex flex-col gap-10">
                     <div className="flex flex-col gap-6">
-                        <div className="rounded-full px-6 py-3 w-fit bg-primaryBlue/25">
+                        <div className="rounded-full px-5 py-2 w-fit bg-primaryBlue/25">
                             <span className="font-poppins text-primaryBlue font-medium">
                                 FEATURE
                             </span>
                         </div>
                         <div className="flex flex-col w-8/12 gap-3">
-                            <h1>Unlock the Power of SleepSpec Technology</h1>
+                            <h1 className="leading-none">
+                                Unlock the Power of SleepSpec Technology
+                            </h1>
                             <p className="text-darkBg">
                                 Experience an efficient sleep health check with
                                 advanced voice analysis, accurate results, and
                                 convenient mobile accessibility.
                             </p>
                         </div>
+                    </div>
+                    <div className="grid grid-cols-3 gap-4">
+                        {featItems.map((item, idx) => (
+                            <div
+                                className="border border-divider/50 rounded-xl pt-4 pb-12 px-4"
+                                key={idx}
+                            >
+                                <img src={item.icon} alt={item.alt} />
+                                <div className="mt-4 flex flex-col gap-3">
+                                    <h2>{item.heading}</h2>
+                                    <p className="text-darkBg">
+                                        {item.paragraph}
+                                    </p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
