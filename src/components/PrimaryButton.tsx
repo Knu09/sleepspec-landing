ba1dabecf1a6b018fc59133bb271cab7c7d0528f
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-25 22:38:17 +0800

    style (PrimaryButton.tsx): add w-fit

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -9,7 +9,7 @@ const PrimaryButton = ({ text, href }: PrimaryButtonProps) => {
     return (
         <a
             href={href}
-            className="z-30 flex flex-row items-center gap-3 px-6 py-3 rounded-full"
+            className="z-30 flex flex-row items-center gap-3 px-6 py-3 rounded-full w-fit"
             id="primary-btn"
             type="button"
         >
