bebaeb2a4f47f889d703997e22fba74bc8649364
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-26 22:17:40 +0800

    style (HighlightOverlay.tsx): adjust position

--- src/components/HighlightOverlay.tsx
+++ src/components/HighlightOverlay.tsx
@@ -7,15 +7,15 @@ const HighlightOverlay = () => {
     return (
         <div className="hidden md:flex bg-white px-5">
             <div className="relative container mx-auto h-96">
-                <div className="absolute top-[10%] left-0 xl:translate-x-1/2 -translate-y-1/2 bg-transparent z-10">
-                    <img src={iphoneFrame} alt="iphone-frame" className="" />
+                <div className="absolute top-[15%] left-0 xl:translate-x-1/2 -translate-y-1/2 bg-transparent z-10 transition-all ease-in-out delay-100 duration-500 hover:scale-105 hover:z-50">
+                    <img src={iphoneFrame} alt="iphone-frame" />
                     <img
                         src={recordPhone}
                         alt="home-phone"
                         className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 -z-10 rounded-2xl shadow-2xl"
                     />
                 </div>
-                <div className="absolute top-[20%] left-[50%] -translate-x-1/2 -translate-y-1/2 bg-transparent z-20">
+                <div className="absolute top-[25%] left-[50%] -translate-x-1/2 -translate-y-1/2 bg-transparent z-20 transition-all ease-in-out delay-100 duration-500 hover:scale-105 hover:z-50">
                     <img src={iphoneFrame} alt="iphone-frame" className="" />
                     <img
                         src={homePhone}
@@ -24,7 +24,7 @@ const HighlightOverlay = () => {
                     />
                 </div>
 
-                <div className="absolute top-[10%] right-0 xl:-translate-x-1/2 -translate-y-1/2 bg-transparent z-10">
+                <div className="absolute top-[15%] right-0 xl:-translate-x-1/2 -translate-y-1/2 bg-transparent z-10 transition-all ease-in-out delay-100 duration-500 hover:scale-105 hover:z-50">
                     <img src={iphoneFrame} alt="iphone-frame" className="" />
                     <img
                         src={languagePhone}
