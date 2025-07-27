e60a210c8165fdb59520b7587ec744e986a49321
Knu09 <132640979+Knu09@users.noreply.github.com>
2025-07-27 16:51:02 +0800

    style (HeroSection): update text, button, and glowing ellipse  responsive to mobile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,10 +4,13 @@ import androidIcon from "../assets/android-icon.png";
 
 const HeroSection = () => {
     return (
-        <div id="hero" className="relative container mx-auto pt-40 pb-72">
-            <div className="flex flex-col text-center gap-14 mx-auto xl:w-8/12 z-30">
+        <div
+            id="hero"
+            className="relative container mx-auto py-40 md:pb-72 px-2 sm:px-4"
+        >
+            <div className="flex flex-col text-center gap-[clamp(32px,5vw,56px)] mx-auto xl:w-8/12 z-30">
                 <div>
-                    <h1>
+                    <h1 className="text-start sm:text-center">
                         Detect Sleep Deprivation Through Your Voice with{" "}
                         <span className="bg-[linear-gradient(to_right,_theme(colors.primaryBlue)_0%,_theme(colors.primaryCyan)_50%,_theme(colors.primary)_100%)] text-transparent bg-clip-text">
                             SleepSpec
@@ -15,14 +18,14 @@ const HeroSection = () => {
                     </h1>
                 </div>
                 <div className="flex flex-col gap-8">
-                    <div className="mx-auto md:w-9/12">
-                        <p>
+                    <div className="mx-auto w-full md:w-9/12">
+                        <p className="text-start sm:text-center">
                             Powered by AI and advanced voice analysis that
                             detects signs of mild sleep deprivation using just
                             your voice.
                         </p>
                     </div>
-                    <div className="flex flex-row justify-center gap-4">
+                    <div className="flex flex-col sm:flex-row justify-center gap-4">
                         <PrimaryButton
                             text="Download APK"
                             href="#"
@@ -33,9 +36,9 @@ const HeroSection = () => {
                 </div>
             </div>
             {/* Glowing Ellipse */}
-            <div className="absolute z-0 left-[50%] top-[230px] w-64 h-64 bg-primary opacity-25 blur-[80px]"></div>
-            <div className="absolute z-0 left-[25%] top-[300px] w-64 h-64 bg-primaryBlue opacity-25 blur-[80px]"></div>
-            <div className="absolute z-0 right-0 top-0 w-40 h-40 bg-[#3AC8D9] opacity-15 blur-[60px]"></div>
+            <div className="hidden md:flex absolute z-0 left-[50%] top-[230px] w-64 h-64 bg-primary opacity-25 blur-[80px]"></div>
+            <div className="absolute z-0 left-[25%] top-[300px] w-40 h-40 sm:w-64 sm:h-64 bg-primaryBlue opacity-25 blur-[80px]"></div>
+            <div className="absolute z-0 right-0 top-0 w-40 h-40 bg-primaryCyan opacity-15 blur-[60px]"></div>
         </div>
     );
 };
